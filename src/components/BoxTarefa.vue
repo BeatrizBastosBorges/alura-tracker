149c95744a96f53d29a2eee985b996ed4c14a628
Beatriz Bastos <biaborges.estudante@gmail.com>
2024-07-17 11:08:03 -0300

    mudança na chamada do estilo

--- src/components/BoxTarefa.vue
+++ src/components/BoxTarefa.vue
@@ -2,18 +2,19 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-    name: 'BoxTarefa'
+    name: 'BoxTarefa',
+    data() {
+        return {
+            estilos: {
+                backgroundColor: '#FAF0CA'
+            }
+        }
+    }
 })
 </script>
 
 <template>
-    <div class="box has-text-weight-bold">
+    <div class="box has-text-weight-bold" :style="estilos">
         <slot></slot>
     </div>
 </template>
-
-<style scoped>
-.box {
-    background: #FAF0CA;
-}
-</style>
